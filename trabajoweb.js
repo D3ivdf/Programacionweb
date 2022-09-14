4244afa21605a442a26f46cb3444cb365050c895
Michelle358 <dceballosc@unal.edu.co>
2022-09-14 12:52:11 -0500

    subir

--- trabajoweb.js
+++ trabajoweb.js
@@ -1,6 +1,6 @@
 function nombre(){
     var nom = document.getElementById("Inputnombre").value;
-    let resultado = nom;
+    let resultado = nom.toUpperCase();
     if(nom.length>0){
         window.alert(`Tu nombre es ${resultado}`)
     }else{
@@ -9,22 +9,49 @@ function nombre(){
 }
 function shows()
 {
-    element = document.getElementById("content");
     radio = document.getElementById("flexRadioDefault1");
+    element = document.getElementById("content");
     if (radio.checked) {
-        element.style.display='block';
+        element.style.display = "block";
+        element.innerHTML=`<div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault">
+         curso web basico
+        </label>
+    </div>
+    <br>
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault2">
+        <label class="form-check-label" for="flexCheckDefault">
+         curso web avansado
+        </label>
+    </div>`;
     }
-    else {
-        element.style.display='none';
+    else{
+        element.innerHTML=``;
     }
 }
 function comparar(){
     let pass1 = document.getElementById("password1").value;
     let pass2 = document.getElementById("password2").value;
     if(pass1 === pass2){
-        window.alert("correcta");
+        document.getElementById("password1").style.borderColor = "green";
+        document.getElementById("password2").style.borderColor = "green";
     }else{
-        window.alert("error");
+        document.getElementById("password1").style.borderColor = "red";
+        document.getElementById("password2").style.borderColor = "red";
     }
 
+}
+function vacio(){
+    let boton= document.getElementById('boton').disabled = false;
+    let correo = document.getElementById('email').value;
+    let email=correo.trim();
+    if((correo.length==0)){
+        document.getElementById('boton').style.borderColor = 'red';
+        document.getElementById('email').disabled=true;
+    }
+    else{
+        document.getElementById('boton').style.borderColor = 'green';
+    }
 }
